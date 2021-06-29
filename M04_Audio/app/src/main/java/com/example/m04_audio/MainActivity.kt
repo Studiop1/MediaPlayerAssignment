0f8ecd06e9422d72a96356b526a9b2a9add3ebc2
Studiop1 <85195210+Studiop1@users.noreply.github.com>
2021-06-29 18:30:16 -0400

    Add files via upload

---
+++ M04_Audio/app/src/main/java/com/example/m04_audio/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.m04_audio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// This assignment is the assignment to do a simple audio media player
+// You have a youtube video but also a 'shortcut' paper to explain how
+// Watch the video and then follow the steps in the paper ( or just
+// follow the video
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
