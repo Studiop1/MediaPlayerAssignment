9fcc7dc4522f67d9b607d06193c4c1479be1745f
Studiop1 <85195210+Studiop1@users.noreply.github.com>
2021-06-29 18:54:51 -0400

    Update MainActivity.kt

--- M04_Audio/app/src/main/java/com/example/m04_audio/MainActivity.kt
+++ M04_Audio/app/src/main/java/com/example/m04_audio/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 // This assignment is the assignment to do a simple audio media player
 // You have a youtube video but also a 'shortcut' paper to explain how
 // Watch the video and then follow the steps in the paper ( or just
-// follow the video
+// follow the video)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
